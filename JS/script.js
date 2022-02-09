a50da00def336b0589aba8fc394b2a539cb9953f
Sergey <sergey@gmail.com>
2022-02-09 23:39:02 +0500

    15

--- JS/script.js
+++ JS/script.js
@@ -33,4 +33,28 @@ if (personalMovieDB.count < 10) {
     console.log("Произошла ошибка");
 }
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+          if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            console.log ('done');
+          } else {
+              console.log ('error');
+              i--;
+          }
+
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+    console.log ("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log ("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log ("Вы киноман");
+} else {
+    console.log ("Произошла ошибка");
+}
+
 console.log(personalMovieDB);
